d14f75eeac148c1ccb20c22b012085b146ca21c5
Beth Urban <beth@mosaicstg.com>
2020-04-21 16:32:51 -0400

    Add custom CSS file and enqueue

--- wp-content/plugins/let_it_rest/letitrest.php
+++ wp-content/plugins/let_it_rest/letitrest.php
@@ -6,4 +6,12 @@
    Author: Beth Urban
    Author URI: http://bethurban.com
    */
+
+   function let_it_rest_scripts() {
+    wp_enqueue_style( 'custom', plugin_dir_path(__FILE__) . 'css/custom.css');   
+   }
+
+   add_action( 'wp_enqueue_scripts', 'let_it_rest_scripts' );
+    
+
 ?>
